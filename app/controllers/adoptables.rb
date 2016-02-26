24d415865f94f2329c346cd6782202298c1f0d98
DaveSpivey <dspive1@gmail.com>
2016-02-26 13:53:33 -0800

    Add city to shelter results

--- app/controllers/adoptables.rb
+++ app/controllers/adoptables.rb
@@ -6,6 +6,9 @@ post '/adoptables' do
   petfinder = Petfinder::Client.new(ENV['PET_KEY'], ENV['PET_SECRET'])
 
   local_pets = petfinder.find_pets('dog', params[:zip])
+  # shelter_id = local_pets.first.shelterid
+  # @shelter = petfinder.shelter(shelter_id)
+
 
   @pets = local_pets.select do |pet|
     pet.breeds.include? params[:breed]
